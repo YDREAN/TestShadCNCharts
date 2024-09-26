948fd660cef4d76d263e9eb670f16b3bad7130d5
YDREAN <yanndrean1@gmail.com>
2024-09-26 11:35:07 +0200

    Rafraichissement des données fonctionnel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,52 @@
-import { ChartArea } from "./chartscomp/chartarea";
+"use client";
+
+import { useEffect, useState } from "react";
+
 import { ChartMongo } from "./chartscomp/chartmongo";
 
-export default async function Home() {
-  const res = await fetch("http://localhost:3000/api/getdatacharts");
+interface ChartData {
+  _id: string;
+  month: string;
+  desktop: number;
+  mobile: number;
+}
 
-  // Vérification si la réponse est OK
-  if (!res.ok) {
-    console.error(
-      "Erreur lors de la récupération des données:",
-      res.statusText
-    );
-    return <div>Erreur lors de la récupération des données</div>;
+export default function Home() {
+  const [testcharts, setTestcharts] = useState<ChartData[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch("http://localhost:3000/api/getdatacharts");
+
+        // Vérification si la réponse est OK
+        if (!res.ok) {
+          throw new Error(
+            "Erreur lors de la récupération des données: " + res.statusText
+          );
+        }
+
+        const data: ChartData[] = await res.json();
+        setTestcharts(data);
+      } catch (err) {
+        setError((err as Error).message);
+      }
+    }
+
+    fetchData();
+  }, []); // Dépendances vides pour exécuter uniquement au montage
+
+  if (error) {
+    return <div>{error}</div>;
   }
-
-  const testcharts = await res.json();
-  console.log(testcharts);
 
   return (
     <>
-      <div className="flex gap-3    items-center justify-center bg-slate-700 w-full h-screen text-white">
+      <div className="flex gap-3 items-center justify-center bg-slate-700 w-full h-screen text-white">
         <div>
-          <h1 className=" text-3xl  font-bold">Données de MongoDB :</h1>
-          <ul className=" m-3">
+          <h1 className="text-3xl font-bold">Données de MongoDB :</h1>
+          <ul className="m-3">
             {testcharts.map((item) => (
               <li key={item._id}>
                 {item.month}: Desktop: {item.desktop}, Mobile: {item.mobile}
